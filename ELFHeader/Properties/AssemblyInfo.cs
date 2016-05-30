991e75c0eb998ed8113bbe45f77dfe7c90370729
unknown <s_david@i08pc23.studpool.informatik.kit.edu>
2016-05-30 12:34:16 +0200

    initial commit

---
+++ ELFHeader/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ELFHeader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Unknown6656")]
+[assembly: AssemblyProduct("ELFHeader")]
+[assembly: AssemblyCopyright("Copyright © 2016, Unknown6656")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en")]
+
+[assembly: ComVisible(true)]
+[assembly: Guid("b66becef-d7dd-4ade-adc7-1a7e731d1eff")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
